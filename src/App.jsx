e78309d42a75e4690fcec0186380f60d16ccb27f
guitarguijames <guitarguijames@gmail.com>
2026-04-23 14:44:31 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Sobre from './pages/Sobre'
-import Solucoes from './pages/Solucoes'
-import Unidades from './pages/Unidades'
-import Contato from './pages/Contato'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Home from './Home'
+import Sobre from './Sobre'
+import Solucoes from './Solucoes'
+import Unidades from './Unidades'
+import Contato from './Contato'
 
 function ScrollToTop() {
   const { pathname } = useLocation()
